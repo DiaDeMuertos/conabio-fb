bc7b6d8ba88987ad0eb611aa6ef1712abaf936b3
DiaDeMuertos <imworkingnow@msn.com>
2019-09-29 14:02:17 -0700

    added basemap and areas mayores

--- front-end/app/src/componets/Map.js
+++ front-end/app/src/componets/Map.js
@@ -1,11 +1,18 @@
-import React from 'react';
-import { Map as LeafletMap, TileLayer, GeoJSON, Marker } from 'react-leaflet';
+import React, { Component } from 'react';
+import {
+  Map as LeafletMap,
+  TileLayer,
+  Marker,
+  GeoJSON,
+  Popup,
+} from 'react-leaflet';
 import './Map.css';
 
 const VERACRUZ_CENTROID = [19.3929572228801, -96.4185585737723];
 
-class Map extends React.Component {
+class Map extends Component {
   render() {
+    const { basemap, poligonos } = this.props;
     return (
       <LeafletMap
         center={VERACRUZ_CENTROID}
@@ -19,7 +26,12 @@ class Map extends React.Component {
         easeLinearity={0.35}
       >
         <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
-        <Marker position={VERACRUZ_CENTROID} />
+        <Marker position={VERACRUZ_CENTROID}>
+          <Popup>Centroide</Popup>
+        </Marker>
+        {basemap && <GeoJSON data={basemap.geojson} />}
+
+        {poligonos && poligonos.map(p => <GeoJSON data={p.geojson} />)}
       </LeafletMap>
     );
   }
