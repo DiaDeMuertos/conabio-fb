240c5eb4646c3ee14225a82f0d87d5a79649faa0
DiaDeMuertos <imworkingnow@msn.com>
2019-09-29 11:48:15 -0700

    added const centroid

--- front-end/app/src/componets/Map.js
+++ front-end/app/src/componets/Map.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { Map as LeafletMap, TileLayer, GeoJSON, Marker } from 'react-leaflet';
 import './Map.css';
 
+const VERACRUZ_CENTROID = [19.3929572228801, -96.4185585737723];
+
 class Map extends React.Component {
   render() {
     return (
       <LeafletMap
-        center={[19.3929572228801, -96.4185585737723]}
+        center={VERACRUZ_CENTROID}
         zoom={6}
         attributionControl={true}
         zoomControl={true}
@@ -17,6 +19,7 @@ class Map extends React.Component {
         easeLinearity={0.35}
       >
         <TileLayer url="http://{s}.tile.osm.org/{z}/{x}/{y}.png" />
+        <Marker position={VERACRUZ_CENTROID} />
       </LeafletMap>
     );
   }
